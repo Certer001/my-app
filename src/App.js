f40f9f8fabcef40e614548d4bc5f183b61ddd3fc
Certer001 <76663981+Certer001@users.noreply.github.com>
2024-01-21 12:12:49 +0300

    lesson 7

--- src/App.js
+++ src/App.js
@@ -4,14 +4,33 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-        <ul>
-            <li>css</li>
-            <li>html</li>
-            <li>js</li>
-            <li>react</li>
-        </ul>
+        <Popa />
+        <Header />
     </div>
   );
 }
+
+const Popa = () => {
+    return(
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+}
+
+const Header = () => {
+    return(
+        <div className="Header">
+            <a>Home</a>
+            <a>page2</a>
+        </div>
+    );
+}
+
 
 export default App;
